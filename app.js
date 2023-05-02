cc686e800db20962fc8815a0be5e06c194b0cd52
Curslo <richardkisivii@gmail.com>
2023-05-02 13:58:52 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+import cors from "cors";
+
+import petRoutes from "./pets/routes/pets.routes.js";
+
+const app = express();
+
+const port = 5000;
+
+//Global middlewares
+app.use(cors());
+
+app.use(express.json());
+
+//Routes
+app.use('pets', petRoutes);
+
+//Server setup
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => console.log(`⚡️[server]: Server is running at https://localhost:${port}`))
+};
+
+export default app
